7fdfecb61f09432fc13af30636109886d4fbe61b
Dimaspdma <dimaspadma@gmail.com>
2023-03-25 22:55:37 +0700

    fix: fix bugs host value
    - Add start scripts
    - Add NODE_ENV
    - Change CORS to *

--- src/server.js
+++ src/server.js
@@ -7,10 +7,10 @@ const init = async () => {
 
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
             cors: {
-                origin: ['http://notesapp-v1.dicodingacademy.com'],
+                origin: ['*'],
         }
     }
     });
